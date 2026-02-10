4a2e1ebe028adae9fac4c189cb3b3f099349c0fe
Bryan Soares <161507572+BryanSoares59d@users.noreply.github.com>
2026-02-10 17:32:52 -0300

    Add files via upload

---
+++ aula_01/index.php
@@ -0,0 +1,10 @@
+<?php
+$nome = "Bryan";
+$idade = 17;
+$preco = 10000000000000000000000000000000000000000000000000000000000000;
+$ativo = true;
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "<br>";
+echo "Preço: R$" . $preco . "<br>";
+echo "Status: " . ($ativo ? "Ativo" : "Inativo");
+?>
